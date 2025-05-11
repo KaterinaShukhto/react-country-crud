7df498ffdd622e3d52b3e11348c590bc3695264a
samsak1990 <samsak.developer@internet.ru>
2025-05-11 16:17:33 +0300

    Added handler for error 404

--- src/API/apiService.tsx
+++ src/API/apiService.tsx
@@ -1,21 +1,28 @@
 import axios from "axios";
 import API_URL from "./API_URL";
+import type { ICountrie } from "../data/Interfaces";
 
-const getAllCountries = async ()=>{
+const getAllCountries = async (): Promise<ICountrie[] | undefined>=>{
     try{
-        const {data} = await axios.get(API_URL)
+        const {data} = await axios.get<ICountrie[]>(API_URL)
         return data
     } catch (err){
         console.error(err)
     }
 }
 
-const getOneCountryById = async (id: number | string)=>{
+const getOneCountryById = async (id: number | string): Promise<ICountrie | number | null> =>{
     try{    
-        const {data} = await axios.get(`${API_URL}/${id}`)
+        const {data} = await axios.get<ICountrie>(`${API_URL}/${id}`)
         return data
     }catch(err){
-        console.error(err)
+        if (axios.isAxiosError(err) && err.response){
+            console.error(`Ошибка HTTP ${err.response.status}: ${err.response.data}`);
+            return err.response.status
+        } else{
+            console.error("Ошибка сети или неизвестная ошибка:", err);
+        }
+        return null; 
     }
 }
 
--- src/components/pages/Details/Details.tsx
+++ src/components/pages/Details/Details.tsx
@@ -4,25 +4,32 @@ import type { ICountrie } from "../../../data/Interfaces";
 import { getOneCountryById } from "../../../API/apiService";
 
 const Details: React.FC = () => {
-  const [counrtyInfo, setCountryInfo] = useState<ICountrie>()
+  const [countryInfo, setCountryInfo] = useState<ICountrie>()
   const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [isNotFound, setIsNotFound] = useState<boolean>(false)
   const navigate = useNavigate()
   const {id} = useParams()
 
   useEffect(()=>{
-    if(id){
-      getOneCountryById(id).then(data=>{
-        setCountryInfo(data)
-      }).catch((error) => console.error("Ошибка загрузки страны:", error))
-      .finally(()=> setIsLoading(false))
-    }
-  }, [])
+    if(!id) return
+    getOneCountryById(id)
+    .then(data=>{
+      if(!data || typeof data === 'number'){
+        setIsNotFound(!isNotFound)
+        return;
+      }
+      setCountryInfo(data)
+    })
+    .catch((error) => console.error("Ошибка загрузки страны:", error))
+    .finally(()=> setIsLoading(false))
+  }, [id])
 
   return (
     <div>
       {isLoading && <p>Загрузка...</p>}
+      {isNotFound && <p>Запрашиваемая страница не найдена</p>}
       <button onClick={()=>navigate(-1)}>Назад</button>
-      <h1>{counrtyInfo?.name}</h1>
+      <h1>{countryInfo?.name}</h1>
     </div>
   );
 };
